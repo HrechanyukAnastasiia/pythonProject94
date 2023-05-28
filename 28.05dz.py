1efe5102ca6098d8d209b4f40786cb8c17690393
Hrechanyuk Anastasiia <anastasiya.grk@gmail.com>
2023-05-28 22:15:37 +0300

    2 завдання

---
+++ 28.05dz.py
@@ -0,0 +1,16 @@
+#2
+def average_closure():
+    numbers = []
+    def add_number(number):
+        numbers.append(number)
+    def get_average():
+        print(numbers)
+        return sum(numbers) / len(numbers)
+    return add_number, get_average
+add_number, get_average = average_closure()
+add_number(1)
+add_number(3)
+add_number(5)
+add_number(7)
+add_number(9)
+print(f"Середнє число зі списку: {get_average()}")
