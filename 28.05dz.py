8aa655b6fc7860bd77d9306f58deda83b82c5104
Hrechanyuk Anastasiia <anastasiya.grk@gmail.com>
2023-05-28 22:12:05 +0300

    1 завдання

---
+++ 28.05dz.py
@@ -0,0 +1,22 @@
+#1
+class PrimeGenerator:
+    def __init__(self):
+        self.index = 2
+    def is_prime(self, num):
+        if num < 2:
+            return False
+        for i in range(2, (num // 2) + 1):
+            if num % i == 0:
+                return False
+        return True
+    def generate(self):
+        new_index = self.index
+        while True:
+            if self.is_prime(new_index):
+                self.index = new_index + 1
+                return new_index
+            new_index += 1
+
+generator = PrimeGenerator()
+for i in range(10):
+    print(generator.generate())
